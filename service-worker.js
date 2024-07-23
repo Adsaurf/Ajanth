8113a6f26710ee94ea6daf5b612b91d73c93034a
Adsaurf <adsaurf.ae@gmail.com>
2024-07-23 21:24:26 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,45 +3,59 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  '/script.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/scripts.js',
+  'https://github.com/Adsaurf/Ajanth/blob/main/icon.png',
+  'https://adsaurf.com/manifest.json'
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
+// Install the service worker
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
+      .then((cache) => {
         return cache.addAll(urlsToCache);
       })
   );
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
+      .then((response) => {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+        return fetch(event.request).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
   );
 });
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  const cacheWhitelist = ['adsaurf-cache-v1'];
+// Update the service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
